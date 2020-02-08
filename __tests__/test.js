b1ca2ae55e40bc506a92486d145a659b2cbdeb73
Saran Tanpituckpong <sarunta@gmail.com>
2020-02-09 02:03:39 +0700

    :white_check_mark: Update tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,12 +1,15 @@
-const { validateConfig } = require('renovate/lib/config/validation');
+const { validateConfig } = require('renovate/dist/config/validation');
 const pkg = require('../package.json');
 
 const configs = pkg['renovate-config'];
 
-test('Renovate config should be valid', () => {
-	Object.keys(configs).forEach(presetName => {
-		const { errors, warnings } = validateConfig(configs[presetName]);
+test('Renovate config should be valid', async () => {
+	for (const presetName in configs) {
+		const config = configs[presetName];
+
+		const { errors, warnings } = await validateConfig(config);
+
 		expect(errors).toHaveLength(0);
 		expect(warnings).toHaveLength(0);
-	});
+	}
 });
